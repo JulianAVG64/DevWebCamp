0dc1e579f1dac8df67586f1a28ba75b1429acb63
JulianAVG64 <julvargasgu@unal.edu.co>
2025-04-03 15:10:10 -0500

    order events in the backend

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,7 +2,12 @@
 
 namespace Controllers;
 
+use Model\Dia;
+use Model\Hora;
 use MVC\Router;
+use Model\Evento;
+use Model\Ponente;
+use Model\Categoria;
 
 class PaginasController {
 
@@ -28,6 +33,37 @@ class PaginasController {
     }
 
     public static function conferencias(Router $router) {
+
+        // Obtener todos los eventos
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+
+        $eventos_formateados = [];
+        foreach($eventos as $evento) {
+            // Cruzar información del modelo para traer las tablas relacionadas
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+
+            // Día viernes categoría conferencias
+            if($evento->dia_id === "1" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+            // Día sábado categoría conferencias
+            if($evento->dia_id === "2" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            // Día viernes categoría workshops
+            if($evento->dia_id === "1" && $evento->categoria_id === "2") {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+            // Día sábado categoría workshops
+            if($evento->dia_id === "2" && $evento->categoria_id === "2") {
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+        }
+        debuguear($eventos_formateados);
 
         $router->render('paginas/conferencias', [
             'titulo' => 'Conferencias & Workshops'
