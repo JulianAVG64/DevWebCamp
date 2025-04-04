d2165897c8f9646304a14c7de5074493f93263f4
JulianAVG64 <julvargasgu@unal.edu.co>
2025-04-04 09:06:31 -0500

    add conferences to index

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -13,8 +13,39 @@ class PaginasController {
 
     public static function index(Router $router) {
 
+        // Obtener todos los eventos
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+
+        $eventos_formateados = [];
+        foreach($eventos as $evento) {
+            // Cruzar información del modelo para traer las tablas relacionadas
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+
+            // Día viernes categoría conferencias
+            if($evento->dia_id === "1" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+            // Día sábado categoría conferencias
+            if($evento->dia_id === "2" && $evento->categoria_id === "1") {
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            // Día viernes categoría workshops
+            if($evento->dia_id === "1" && $evento->categoria_id === "2") {
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+            // Día sábado categoría workshops
+            if($evento->dia_id === "2" && $evento->categoria_id === "2") {
+                $eventos_formateados['workshops_s'][] = $evento;
+            }
+        }
+
         $router->render('paginas/index', [
-            'titulo' => 'Inicio'
+            'titulo' => 'Inicio',
+            'eventos' => $eventos_formateados
         ]);
     }
 
