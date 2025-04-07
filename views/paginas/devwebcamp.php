93a48ffac575bdf182b53f5783463792702ecad4
JulianAVG64 <julvargasgu@unal.edu.co>
2025-04-06 19:17:54 -0500

    add php random aos-animations function

--- views/paginas/devwebcamp.php
+++ views/paginas/devwebcamp.php
@@ -3,7 +3,7 @@
     <p class="devwebcamp__descripcion">Conoce la conferencia mas importante de Latinoamérica</p>
 
     <div class="devwebcamp__grid">
-        <div class="devwebcamp__imagen">
+        <div <?php aos_animation(); ?> class="devwebcamp__imagen">
             <picture>
                 <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
                 <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
@@ -12,9 +12,9 @@
         </div>
 
         <div class="devwebcamp__contenido">
-            <p class="devwebcamp__texto">Donec vel tincidunt ligula, eu ornare enim. Phasellus eu augue iaculis, luctus orci eu, elementum dolor. Integer egestas cursus urna. Aliquam vel fringilla sem. Pellentesque eget ligula fringilla, ullamcorper lacus sit amet, maximus sem. Fusce viverra consequat venenatis. Nullam eget ante tellus.</p>
+            <p <?php aos_animation(); ?> class="devwebcamp__texto">Donec vel tincidunt ligula, eu ornare enim. Phasellus eu augue iaculis, luctus orci eu, elementum dolor. Integer egestas cursus urna. Aliquam vel fringilla sem. Pellentesque eget ligula fringilla, ullamcorper lacus sit amet, maximus sem. Fusce viverra consequat venenatis. Nullam eget ante tellus.</p>
             
-            <p class="devwebcamp__texto">Donec vel tincidunt ligula, eu ornare enim. Phasellus eu augue iaculis, luctus orci eu, elementum dolor. Integer egestas cursus urna. Aliquam vel fringilla sem. Pellentesque eget ligula fringilla, ullamcorper lacus sit amet, maximus sem. Fusce viverra consequat venenatis. Nullam eget ante tellus.</p>
+            <p <?php aos_animation(); ?> class="devwebcamp__texto">Donec vel tincidunt ligula, eu ornare enim. Phasellus eu augue iaculis, luctus orci eu, elementum dolor. Integer egestas cursus urna. Aliquam vel fringilla sem. Pellentesque eget ligula fringilla, ullamcorper lacus sit amet, maximus sem. Fusce viverra consequat venenatis. Nullam eget ante tellus.</p>
         </div>
     </div>
 </main>
