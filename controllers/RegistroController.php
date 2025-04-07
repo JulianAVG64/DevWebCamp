5b053457e06987ea23dd09b981b8e588576d18d3
JulianAVG64 <julvargasgu@unal.edu.co>
2025-04-07 16:22:20 -0500

    avoid duplicate records

--- controllers/RegistroController.php
+++ controllers/RegistroController.php
@@ -11,6 +11,16 @@ class RegistroController {
 
     public static function crear(Router $router) {
 
+        if(!is_auth()) {
+            header('Location: /');
+        }
+
+        // Verificar si el usuario ya tiene un registro
+        $registro = Registro::where('usuario_id', $_SESSION['id']);
+        if(isset($registro) && $registro->paquete_id === "3") {
+            header('Location: /boleto?id=' . urlencode($registro->token));
+        }
+
         $router->render('registro/crear', [
             'titulo' => 'Finalizar Registro'
         ]);
@@ -21,6 +31,12 @@ class RegistroController {
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             if(!is_auth()) {
                 header('Location: /login');
+            }
+
+            // Verificar si el usuario ya tiene un registro
+            $registro = Registro::where('usuario_id', $_SESSION['id']);
+            if(isset($registro) && $registro->paquete_id === "3") {
+                header('Location: /boleto?id=' . urlencode($registro->token));
             }
 
             // Generar token de registro
